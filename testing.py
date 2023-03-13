eb4593ef8ec4d449dbc6e037be5293ccaee19cfe
Brayan Esteven Bernal Martínez <100706145+LuffyBB1@users.noreply.github.com>
2023-03-12 23:13:40 -0500

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-from funciones import menor
-def test_menor():
-    listas=[1,2,65,4,,15,1.1]
-    ls=[0,2,4,6,6]
-    a=[0.5,64,,44,1,31,3]
-    
-    assert menor(lsitas)==1.1
-    assert menor(a)==0.5
-    assert menor(ls)==0
